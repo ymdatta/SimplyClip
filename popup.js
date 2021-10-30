a124c92b3ef884fdedfbefd4467837bc15f58288
niveditalodha <niveditalodha1234@gmail.com>
2021-10-30 15:54:29 -0400

    Function for download as CSV

--- popup.js
+++ popup.js
@@ -50,7 +50,6 @@ function getClipboardText() {
             })
             if (typeof list !== undefined)
                 list.forEach(item => {
-                    console.log(item);
                     addClipboardListItem(item)
                 });
         }
@@ -215,14 +214,15 @@ function downloadClipboardText(){
             console.log("Nothing to download")
         }
         else {
-            var list_of_items = ""
+            var list_of_items = []
             emptyDiv.classList.add('hide-div');
             if (typeof list !== undefined){
                 list.forEach(item => {
                     addClipboardListItem(item)
+                    // list_of_items.push(...[item])
                     list_of_items = list_of_items + item + "\n\n"
-                    console.log(item)   
                 });
+                // exportToCsv('SimplyClip.csv',list_of_items)
                 var link, blob, url;
                 blob = new Blob(['\ufeff', list_of_items], {
                     type: 'application/msword'
@@ -241,6 +241,49 @@ function downloadClipboardText(){
         }
     });
 
+}
+
+function exportToCsv(filename, rows) {
+    var processRow = function (row) {
+        var finalVal = '';
+        for (var j = 0; j < row.length; j++) {
+            var innerValue = row[j] === null ? '' : row[j].toString();
+            if (row[j] instanceof Date) {
+                innerValue = row[j].toLocaleString();
+            };
+            console.log(innerValue)
+            var result = innerValue.replace(/"/g, '""');
+            result = result.replace(/,/g,'')
+            result = result.replace(/\n/g,'')
+            result = result.replace(/\r/g,'')
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            finalVal += result;
+        }
+        console.log(finalVal)
+        return finalVal + '\n';        
+    };
+
+    var csvFile = 'SimplyClip Text!!\n\n';
+    for (var i = 0; i < rows.length; i++) {
+        csvFile += processRow(rows[i]);
+    }
+    console.log(csvFile)
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) {
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
 }
 
 function searchClipboardText() {
