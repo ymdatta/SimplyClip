599cc7b182aed794e65105399329152d86810bd7
Neha Kotcherlakota <nkotche@ncsu.edu>
2021-11-03 21:37:16 -0400

    Added comments in the test file

--- seleniumTest.js
+++ seleniumTest.js
@@ -2,92 +2,116 @@
 const assert = require('assert');
 let webdriver = require("selenium-webdriver");
 let chrome = require("selenium-webdriver/chrome");
-let fs = require('fs');
 const {Key,
-    By
-} = require("selenium-webdriver");
-const {Action,
-    Actions
-} = require("selenium-webdriver/lib/input");
+    By} = require("selenium-webdriver");
 
 
-
-// eslint-disable-next-line no-undef
 describe('Verify setup with Google Search',function() {
-    // eslint-disable-next-line no-undef
     it('browser should open', async function () {
+
+        // Open the Chrome Browser with a custom profile
         const options = new chrome.Options()
             .addArguments('--user-data-dir=/Users/nehakotcherlakota/Desktop');
 
-
+        // Initialise driver to launch Chrome
         const driver = new webdriver.Builder()
             .forBrowser('chrome')
             .setChromeOptions(options)
             .build();
-
+        // Launch Google.com
         driver.get('http://google.com');
+
+        // Search for abc in the searchbox in Chrome and Press Enter
         const searchBox = driver.findElement(webdriver.By.name('q'));
         searchBox.sendKeys('abc', Key.RETURN);
 
+        // Check if the value in the searchbox is equal to the value you entered
         searchBox.getAttribute('value').then(function(value) {
             assert.equal(value, 'abc');
         });
+
+        // Close the browser
         driver.close();
+
+        // Quit the browser
         driver.quit();
     });
 });
 
-// eslint-disable-next-line no-undef
+
 describe('Check browser copy functionality',function() {
-    // eslint-disable-next-line no-undef,no-empty-function
     it('text should be copied', async function () {
+
+        // Open the Chrome Browser with a custom profile
         const options = new chrome.Options()
             .addArguments('--user-data-dir=/Users/nehakotcherlakota/Desktop');
 
-
+        // Initialise driver to launch Chrome
         const driver = new webdriver.Builder()
             .forBrowser('chrome')
             .setChromeOptions(options)
             .build();
 
+        // Launch Google.com
         driver.get('http://google.com');
+
+        // Search for abc in the searchbox in Chrome and Press Enter
         const searchBox = driver.findElement(webdriver.By.name('q'));
         searchBox.sendKeys('hello', Key.RETURN);
+
+        // Store the text in the first div in the search results page
         let results = driver.findElement(By.xpath("html/body/div[1]/div[5]/div[4]/div[5]/div[1]/div[1]/div/div/div"));
+
+        // Check if the value is stored
         results.getAttribute('value').then(function(value) {
             assert.equal(value, results.getText());
         });
 
+        // Close the browser
         driver.close();
+
+        // Quit the browser
         driver.quit();
     }).timeout(10000);
 });
 
-// eslint-disable-next-line no-undef,no-empty-function
 describe('Check simply clip functionality',function() {
-    // eslint-disable-next-line no-undef,no-empty-function
     it('copied text should exist in SimplyClip clipboard', async function () {
+        // Open the Chrome Browser with a custom profile
         const options = new chrome.Options()
             .addArguments('--user-data-dir=/Users/nehakotcherlakota/Desktop');
 
-
+        // Initialise driver to launch Chrome
         const driver = new webdriver.Builder()
             .forBrowser('chrome')
             .setChromeOptions(options)
             .build();
 
+        // Launch Google.com
         driver.get('http://google.com');
+
+        // Search for abc in the searchbox in Chrome and Press Enter
         const searchBox = driver.findElement(webdriver.By.name('q'));
         searchBox.sendKeys('hello', Key.RETURN);
+
+        // Store the text in the first div in the search results page
         let results = driver.findElement(By.xpath("html/body/div[1]/div[5]/div[4]/div[5]/div[1]/div[1]/div/div/div"));
-        results.getAttribute('value').then(function(value) {
-        });
+
+        //Execute the Command+C command to copy the text in the first div in the search results page
+        results.sendKeys(Key.COMMAND, 'c');
+
+        //Retrieve the result from the clipboard list in the extension
         let clipboard_result = driver.findElement(By.className("clipboard_list"));
+
+        //Check if the copied value exists in the clipboard list of the extension
         clipboard_result.getAttribute('value').then(function(value) {
             assert.equal(value, results.getText());
         });
 
+        // Close the browser
         driver.close();
+
+        // Quit the browser
         driver.quit();
     }).timeout(10000);
 });
