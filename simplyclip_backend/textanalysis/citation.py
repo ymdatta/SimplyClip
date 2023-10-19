5fd61d9c273a35f55ee4cb3d6042e0666121d285
Mohan Yelugoti <myelugo@ncsu.edu>
2023-10-18 23:22:19 -0400

    add debug statement to get an idea of when citation generation is completed
    Signed-off-by: Mohan Yelugoti <myelugo@ncsu.edu>

--- simplyclip_backend/textanalysis/citation.py
+++ simplyclip_backend/textanalysis/citation.py
@@ -10,6 +10,7 @@ def generate_citation(text):
     if len(text) > 300:
         # Mark it as a URL
         citation_text = "Can't cite: This doesn't look like a DOI. Please check!"
+        print("Citation generation completed")
         return [citation_text]
 
     try:
@@ -18,12 +19,15 @@ def generate_citation(text):
             citation_text += ":\n"
             citation_text += habanero.cn.content_negotiation(ids=text, format='text', style=citation) 
             citation_text += "\n"
+
+        print("Citation generation completed")
     except Exception as e:
         raise CitationError("Error in citation generation: " + str(e))
     except:
         citation_text = ""
         citation_text += "URL: \n"
         citation_text += text
+        print("Citation generation completed")
 
     output_list = [citation_text]
     return output_list
